b174772c320b009852ff1155c2f9e633840ad147
Erik Hollensbe <erik@hollensbe.org>
2010-05-29 20:30:27 -0400

    * More tests and less failing with the file management.

--- test/test_database.rb
+++ test/test_database.rb
@@ -1,11 +1,16 @@
 require 'helper'
 
 class TestDatabase < Test::Unit::TestCase
-  def test_01_works
+  def test_01_connect 
     dbh = new_database 
     assert(dbh)
     assert_kind_of(RDBI::Driver::SQLite3::Database, dbh)
     assert_kind_of(RDBI::Database, dbh)
     assert_equal(dbh.database_name, ":memory:")
+  end
+
+  def test_02_ping
+    assert_equal(0, RDBI.ping(:SQLite3, :database => ":memory:"))
+    assert_equal(0, new_database.ping)
   end
 end
